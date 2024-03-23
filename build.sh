64e6d0b439b6b2fb126fbae5bee178c0ac00dc21
Ryan Fauzi <76712935+ryanfauzi1@users.noreply.github.com>
2024-03-23 22:59:05 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ function build_kernel() {
     echo -e "\n"
 
     make -j$(nproc --all) O=out ARCH=arm64 vendor/ginkgo-perf_defconfig
-    make -j$(nproc --all) ARCH=arm64 O=out \
+    make -j$(nproc --all) ARCH=arm64 O=out CC=clang \
                           CROSS_COMPILE=aarch64-linux-gnu- \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                           CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
